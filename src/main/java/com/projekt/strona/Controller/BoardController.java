53c9cda978329f9568f5e755377f50bcd2096cb7
Tomasz <tomalek0@tlen.pl>
2018-12-07 20:40:49 +0100

    Tutorial01: tworzenie katalogów dla Springa, kontroler oraz html dla strony głównej

---
+++ src/main/java/com/projekt/strona/Controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.projekt.strona.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+    @RequestMapping(value = "/")
+    public String showIndex(){
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/projekt/strona/StronaApplication.java
@@ -0,0 +1,12 @@
+package com.projekt.strona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaApplication.class, args);
+    }
+}
